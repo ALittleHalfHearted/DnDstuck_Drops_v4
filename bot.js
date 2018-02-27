925a318dd874079a2641a3578adf35c7bd8ec387
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-02-26 20:55:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    if (message.substring(0,1) == '!') {
+        message.reply('ping');
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
