6140dbd1acf7c6d10c89b71c40d8ab0e40b1e6b4
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-03-01 14:17:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('message', message => {
 	if (message.content === 'ping') {
 		message.reply('pong');
 	}
-	if (message.content.substring(0,1) === '!') {
+	if (message.content.substring(0,1) === '%') {
 		var args = message.content.substring(1).split(' ');
 		var cmd = args[0];
 		args = args.splice(1);
@@ -38,33 +38,33 @@ client.on('message', message => {
 		var health = 0;
 		
 		switch(cmd) {
-			// !ping
+			// %ping
 			case 'ping':
 				message.reply('Pong!');
 			break;
 			case 'drops':
 				var command = message.content.substring(7);
 				switch(command){
-					//![enemy] [#killed]
+					//%[enemy] [#killed]
 					case 'imp': case 'ogre': case 'basilisk': case 'lich': case 'giclops': case 'lich': case 'giclops': case 'titachnid': case 'archeron': case 'rook':
 						message.reply('**```Use this command to get drops from any number of a single type of enemy. Automatically divides grist ' +
-							      'and applies multipliers.```**\n\n**Format:** `![enemy name] [# killed]`\n\n**examples:**\n`!ogre 45` gets drops from 45 ' +
-							      'ogres\n`!rook 22` gets drops from 22 rooks.');
+							      'and applies multipliers.```**\n\n**Format:** `%[enemy name] [# killed]`\n\n**examples:**\n`%ogre 45` gets drops from 45 ' +
+							      'ogres\n`%rook 22` gets drops from 22 rooks.');
 					break;
-					//!multi t[#] [#killed] t[#] [# killed](Repeat as necessary)
+					//%multi t[#] [#killed] t[#] [# killed](Repeat as necessary)
 					case 'multi':
 						message.reply('**```Use this command to get drops from any number of any amount of enemy types. Automatically divides ' +
 						'grist and applies multipliers.```**\n```Tier #s\nt1 = imp\nt2 = ogre\nt3 = basilisk\nt4 = lich\nt5 = giclops\nt6 ' +
-						'= titachnid\nt7 = archeron\nt8 = rook/D.A.```\n\n**Format:** `!multi t[tier #] [#killed] t[tier #] [# killed](Repeat as ' +
-						'necessary)`\n\n**examples:**\n`!multi t3 54 t8 3` gets drops from 54 basilisks and 3 rooks\n`!multi t6 20 t2 8` gets drops ' +
+						'= titachnid\nt7 = archeron\nt8 = rook/D.A.```\n\n**Format:** `%multi t[tier #] [#killed] t[tier #] [# killed](Repeat as ' +
+						'necessary)`\n\n**examples:**\n`%multi t3 54 t8 3` gets drops from 54 basilisks and 3 rooks\n`%multi t6 20 t2 8` gets drops ' +
 						'from 20 titachnids and 8 ogres.');
 					break;
-					//!custom [# killed] [Tier] [# of boon dice]d[dice value] [# of grist dice]d[dice value]
+					//%custom [# killed] [Tier] [# of boon dice]d[dice value] [# of grist dice]d[dice value]
 					case 'custom':
 						message.reply('**```Use this command to get drops from any number of any enemy that doesn\'t have a command. ' +
-							      'Automatically divides grist and applies multipliers according to the tier.```**\n\n**Format:** `!custom [# ' +
+							      'Automatically divides grist and applies multipliers according to the tier.```**\n\n**Format:** `%custom [# ' +
 							      'killed] t[Tier (1-7 (UNDEFINED: 9))] [# of boon dice]d[dice value] [# of grist dice]d[dice ' +
-							      'value]`\n\n**examples:**\n`!custom 14 t2 200d40 88d94` rolls 200d40 for boon and 88d94 for grist, then multiplies ' +
+							      'value]`\n\n**examples:**\n`%custom 14 t2 200d40 88d94` rolls 200d40 for boon and 88d94 for grist, then multiplies ' +
 							      'and breaks down as if it were an ogre. You\'ll have to do health drops yourself.');
 					break;
 					case 'death':
@@ -72,9 +72,9 @@ client.on('message', message => {
 					break;
 					//normal
 					default:
-						message.reply('use `!drops [command]` to get info on a specific command\n\n```To get' +
-							      ' drops:```\n`!imp`\n`!ogre`\n`!basilisk`\n`!lich`\n`!giclops`\n`!' +
-							      'titachnid`\n`!archeron`\n`!rook`\n`!multi`\n`!custom`\n\n```Other:```\n`!death`');
+						message.reply('use `%drops [command]` to get info on a specific command\n\n```To get' +
+							      ' drops:```\n`%imp`\n`%ogre`\n`%basilisk`\n`%lich`\n`%giclops`\n`%' +
+							      'titachnid`\n`%archeron`\n`%rook`\n`%multi`\n`%custom`\n\n```Other:```\n`%death`\n`%check (WIP)`');
 				}
 			break;
 			case 'death':
@@ -469,8 +469,8 @@ client.on('message', message => {
 				}
 			break;
 			case 'custom':
-				//!custom [# killed] [Tier] [# of boon dice]d[dice value] [# of grist dice]d[dice value]
-				//!custom 20 9 100d100 100d100
+				//%custom [# killed] [Tier] [# of boon dice]d[dice value] [# of grist dice]d[dice value]
+				//%custom 20 9 100d100 100d100
 				var num = message.content.substring(8,10)
 				var tier = message.content.substring(message.content.indexOf('t',9) + 1,message.content.indexOf('t',9) + 2)
 				var boonDieVal = message.content.substring(message.content.indexOf('d') + 1, message.content.indexOf(' ',message.content.indexOf('d')))
