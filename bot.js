c7063b2546a1fad3dc931635987d16967335b1b7
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-03-13 14:37:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log('I am ready!');
-	client.user.game('%drops for help');
+	client.user.setGame('%drops for help');
 });
 
 
