1578725d0864e59afe23077d355e0e1c4b062f91
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-03-16 10:01:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,7 +166,7 @@ client.on('message', message => {
 						var op = ""
 					}
 					let calculate = "=" + math + op + message.content.toLowerCase().substring('%check '.length);
-					math = calculate;
+					math = calculate.replace("=","") + "=";
 					if (isFinite(calculate.replace(/\=|\+|\-|\*|\/|\÷|\%|\(|\)|\,|\ |math.|pow|sqrt|round|floor|ceiling|ceil|pi|π|euler|absolute|abs|exp|logarithm|log|random|rand|rng/g,''))) {
 						calculate = calculate.replace(/ /g, "").replace(/÷/g, "/").replace(/power|pow/g, "Math.pow").replace(/sqrt|squareroot/g, "Math.sqrt").replace(/round/g, "Math.round").replace(/floor/g, "Math.floor").replace(/ceiling|ceil/g, "Math.ceil").replace(/pi|π/g, "Math.PI").replace(/euler/g, "Math.E").replace(/absolute|abs/g, "Math.abs").replace(/exp/g, "Math.exp").replace(/logarithm|log/g, "Math.log").replace(/random|rand|rng/g, "Math.random()");/*.replace(/acos|arccosine/g, "Math.acos").replace(/asin|arcsine/g, "Math.asin").replace(/atan|arctangent|atan1|arctangent1/g, "Math.atan").replace(/atan2|arctangent2/g, "Math.atan2").replace(/cos|cosine/g, "Math.cos").replace(/sin|sine/g, "Math.sin").replace(/tan|tangent/g, "Math.tan")*/;
 						if (calculate.replace(/[^%]/g, "").length > 0) {
@@ -186,7 +186,7 @@ client.on('message', message => {
 					}
 					check = eval(calculate);
 				}
-				message.reply('Your check resulted in: ' + check + '\nCheck math: ' + math);
+				message.reply('Your check resulted in: ' + math + check);
 			break;
 			case 'imp':
 				var x = message.content.substring(5);
