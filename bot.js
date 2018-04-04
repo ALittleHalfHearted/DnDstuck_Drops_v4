e5e7f31b18d4bf1299576c1917df0f346e939ea4
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-04-04 12:53:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ client.on('message', message => {
 	if (message.content.substring(0,1) === '%') {
 		var args = message.content.substring(1).split(' ');
 		var cmd = args[0];
-		args = args.splice(1);
-		args = args.toString();
+		args = args.splice(1).toString();
+		args = args.replace(/,/g,' ');
 		
 		var check = 0;
 		var check2 = 0;
