a63aba5df3a57dc13e679a887bbb6a017bc2e5f0
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-04-24 10:20:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ const embed = new Discord.RichEmbed()
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-	console.log('Online at\n-----' + d.toString().replace('UTC','CDT').replace('+0000','-0500') + '\n-----' + UTC);
+	console.log('Online at\n-----' + d.toString().replace('UTC','CDT').replace('+0000','-0500') + '\n-----' + UTC.toString().replace('+','±'));
 	client.user.setActivity('%drops for help');
 });
 
