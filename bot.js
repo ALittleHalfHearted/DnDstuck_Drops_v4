a82498631fea2347a7164e3441dda5261ac2ec7a
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-05-16 18:32:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -637,112 +637,195 @@ client.on('message', message => {
 					overload(message);
 				}
 			break;
-			case 'custom':
-				//%custom [# killed] [Tier] [# of boon dice]d[dice value] [# of grist dice]d[dice value]
-				//%custom 20 9 100d100 100d100
-				var num = message.content.substring(8,10)
-				var tier = message.content.substring(message.content.indexOf('t',9) + 1,message.content.indexOf('t',9) + 2)
-				var boonDieVal = message.content.substring(message.content.indexOf('d') + 1, message.content.indexOf(' ',message.content.indexOf('d')))
-				var boonDieNum = message.content.substring(13,message.content.indexOf('d'))
-				var gristDieVal = message.content.substring(message.content.lastIndexOf('d') + 1)
-				var gristDieNum = message.content.substring(message.content.lastIndexOf(' ') + 1,message.content.lastIndexOf('d'))
-				
-				if(num > 40 || tier > 9 || boonDieVal > 100 || boonDieNum > 1000 || gristDieVal > 100 || gristDieNum > 1000){
-					message.reply('You need to keep this within reason... The most I will work with is:\n`20 t9 1000d100 1000d100`')
+			case 'multi':
+				var highest = 0
+				if(message.content.indexOf('t1') != -1){
+					imps = message.content.substring(message.content.indexOf('t1') + 2,message.content.indexOf('t1') + 4);
+					highest = 1;
+					mod = 1;
 				}
-				else if(num == 0 || tier == 0 || boonDieVal == 0 || boonDieNum == 0 || gristDieVal == 0 || gristDieNum == 0){
-					message.reply('Please for the love of god fill everything in.')
+				if(message.content.indexOf('t2') != -1){
+					ogres = message.content.substring(message.content.indexOf('t2') + 2,message.content.indexOf('t2') + 4);
+					highest = 2;
+					mod = 2;
 				}
-				else if(tier > 8){
-					message.reply('Your tier is undefined. Please wait until my creator has figured this out.')
+				if(message.content.indexOf('t3') != -1){
+					basilisks = message.content.substring(message.content.indexOf('t3') + 2,message.content.indexOf('t3') + 4);
+					highest = 3;
+					mod = 4;
+				}
+				if(message.content.indexOf('t4') != -1){
+					liches = message.content.substring(message.content.indexOf('t4') + 2,message.content.indexOf('t4') + 4);
+					highest = 4;
+					mod = 8;
+				}
+				if(message.content.indexOf('t5') != -1){
+					giclopes = message.content.substring(message.content.indexOf('t5') + 2,message.content.indexOf('t5') + 4);
+					highest = 5;
+					mod = 16;
+				}
+				if(message.content.indexOf('t6') != -1){
+					titachnids = message.content.substring(message.content.indexOf('t6') + 2,message.content.indexOf('t6') + 4);
+					highest = 6;
+					mod = 32;
+				}
+				if(message.content.indexOf('t7') != -1){
+					archerons = message.content.substring(message.content.indexOf('t7') + 2,message.content.indexOf('t7') + 4);
+					highest = 7;
+					mod = 64;
+				}
+				if(message.content.indexOf('t8') != -1){
+					rooks = message.content.substring(message.content.indexOf('t8') + 2,message.content.indexOf('t8') + 4);
+					highest = 8;
+					mod = 128;
+				}				
+				//don't run if there's no input
+				if(highest == 0){
+					message.reply('why don\'t you have input ~~you little shit~~');
 				}
 				else{
-					for(var i = 0; i < num; i++){
-						for(var z = 0; z < gristDieNum; z++){
-							grist = grist + Math.floor(Math.random() * gristDieVal) + 1;
+					//imp drops
+					if(imps > 0){
+						for(var i = 0; i < imps; i++){
+							grist = impGrist(grist);
+							boon = impBoon(boon);
+							health = impHealth(health);
 						}
-						for(var y = 0; y < boonDieNum; y++){
-							boon = boon + Math.floor(Math.random() * boonDieNum) + 1;
+						build = build + (grist * 0.8);
+						t1 = t1 + (grist * 0.2);
+						grist = 0;
+						
+					}
+				
+					//ogre drops
+					if(ogres > 0){
+						for(var i = 0; i < ogres; i++){
+							grist = ogreGrist(grist);
+							boon = ogreBoon(boon);
+							health = ogreHealth(health);
 						}
+						build = build + (grist * 0.6)
+						t1 = t1 + (grist * 0.3)
+						t2 = t2 + (grist * 0.1)
+						grist = 0
+					}
+				
+					//basilisk drops
+					if(basilisks > 0){
+						for(var i = 0; i < basilisks; i++){
+							//5d100
+							grist = basiliskGrist(grist);
+							//1d100
+							boon = basiliskBoon(boon);
+							health = basiliskHealth(health);
+						}
+						build = build + (grist * 0.4); 
+						t1 = t1 + (grist * 0.3)
+						t2 = t2 + (grist * 0.2)
+						t3 = t3 + (grist * 0.1)
+						grist = 0
+					}
+				
+					//lich drops
+					if(liches > 0){
+						for(var i = 0; i < liches; i++){
+							//10d100
+							grist = lichGrist(grist);
+							//2d100
+							boon = lichBoon(boon);
+							health = lichHealth(health);
+						}
+						build = build + (grist * 0.3)
+						t1 = t1 + (grist * 0.2)
+						t2 = t2 + (grist * 0.2)
+						t3 = t3 + (grist * 0.2)
+						t4 = t4 + (grist * 0.1)
+						grist = 0
+					}
+				
+					//giclops drops
+					if(giclopes > 0){
+						for(var i = 0; i < giclopes; i++){
+							//15d100
+							grist = giclopsGrist(grist);
+							//4d100
+							boon = giclopsBoon(boon);
+							health = giclopsHealth(health);
+						}
+						build = build + (grist * 0.25)
+						t1 = t1 + (grist * 0.20)
+						t2 = t2 + (grist * 0.15)
+						t3 = t3 + (grist * 0.15)
+						t4 = t4 + (grist * 0.1)
+						t5 = t5 + (grist * 0.05)
+						grist = 0
 					}
 					
-					//20% T0 10% t1 10% T2 10% T3 10% T4 10% T5 10% T6 10% T7 10% T8 
-					if(tier == 8){
-						var build = (grist * 128) * 0.2
-						var t1 = (grist * 128) * 0.1
-						var t2 = (grist * 128) * 0.1
-						var t3 = (grist * 128) * 0.1
-						var t4 = (grist * 128) * 0.1
-						var t5 = (grist * 128) * 0.1
-						var t6 = (grist * 128) * 0.1
-						var t7 = (grist * 128) * 0.1
-						var t8 = (grist * 128) * 0.1
-					}
-					if(tier == 7){
-						var build = (grist * 64) * 0.2
-						var t1 = (grist * 64) * 0.15
-						var t2 = (grist * 64) * 0.15
-						var t3 = (grist * 64) * 0.1
-						var t4 = (grist * 64) * 0.1
-						var t5 = (grist * 64) * 0.1
-						var t6 = (grist * 64) * 0.1
-						var t7 = (grist * 64) * 0.1
-						var boon = boon * 64
-					}
-					if(tier == 6){
-						var build = (grist * 32) * 0.2
-						var t1 = (grist * 32) * 0.2
-						var t2 = (grist * 32) * 0.15
-						var t3 = (grist * 32) * 0.15
-						var t4 = (grist * 32) * 0.15
-						var t5 = (grist * 32) * 0.15
-						var t6 = (grist * 32) * 0.15
-						boon = boon * 32
-					}
-					if(tier == 5){
-						var build = (grist * 16) * 0.25
-						var t1 = (grist * 16) * 0.20
-						var t2 = (grist * 16) * 0.15
-						var t3 = (grist * 16) * 0.15
-						var t4 = (grist * 16) * 0.1
-						var t5 = (grist * 16) * 0.05
-						boon = boon * 16
-					}
-					if(tier == 4){
-						var build = (grist * 8) * 0.3
-						var t1 = (grist * 8) * 0.2
-						var t2 = (grist * 8) * 0.2
-						var t3 = (grist * 8) * 0.2
-						var t4 = (grist * 8) * 0.1
-						boon = boon * 8
-					}
-					if(tier == 3){
-						var build = (grist * 4) * 0.4; 
-						var t1 = (grist * 4) * 0.3
-						var t2 = (grist * 4) * 0.2
-						var t3 = (grist * 4) * 0.1
-						boon = boon * 4
-					}
-					if(tier == 2){
-						var build = (grist * 2) * 0.6
-						var t1 = (grist * 2) * 0.3
-						var t2 = (grist * 2) * 0.1
-						boon = boon * 2
-					}
-					if(tier == 1){
-						var build = grist * 0.8
-						var t1 = grist * 0.2
+					//titachnid drops
+					if(titachnids > 0){
+						for(var i = 0; i < titachnids; i++){
+							//25d100
+							grist = titachnidGrist(grist);
+							//10d100
+							boon = titachnidBoon(boon);
+							health = titachnidHealth(health);
+						}
+						build = build + (grist * 0.2)
+						t1 = t1 + (grist * 0.2)
+						t2 = t2 + (grist * 0.15)
+						t3 = t3 + (grist * 0.15)
+						t4 = t4 + (grist * 0.15)
+						t5 = t5 + (grist * 0.15)
+						t6 = t6 + (grist * 0.15)
+						grist = 0
 					}
 					
-					message.reply('```for killing ' + num + ' enemies of tier ' + tier + ', dropping a total of' + boonDieNum + 'd' + boonDieVal + ' ' +
-						'boon and ' + gristDieNum + 'd' + gristDieVal + ' grist per enemy, you have obtained:\nBoon = ' + boon + '\nBG = ' + build.toFixed(0) +
-						'\nT1 = ' + t1.toFixed(0) + '\nT2 = ' + t2.toFixed(0) + '\nT3 = ' + t3.toFixed(0) + '\nT4 = ' + t4.toFixed(0) + '\nT5 = ' +
-						t5.toFixed(0) + '\nT6 = ' + t6.toFixed(0) + '\nT7 = ' + t7.toFixed(0) + '\nT8 = ' + t8.toFixed(0) + '\nT9 = ' + t9.toFixed(0) + '```');
+					//archeron drops
+					if(archerons > 0){
+						for(var i = 0; i < archerons; i++){
+							//40d100
+							grist = archeronGrist(boon);
+							//20d100
+							boon = archeronBoon(boon);
+							health = archeronHealth(health);
+						}
+						build = build + (grist * 0.2)
+						t1 = t1 + (grist * 0.15)
+						t2 = t2 + (grist * 0.15)
+						t3 = t3 + (grist * 0.1)
+						t4 = t4 + (grist * 0.1)
+						t5 = t5 + (grist * 0.1)
+						t6 = t6 + (grist * 0.1)
+						t7 = t7 + (grist * 0.1)
+						grist = 0
+					}
+					
+					//rook drops
+					if(rooks > 0){
+						for(var i = 0; i < rooks; i++){
+							//50d100
+							boon = rookBoon(boon);
+							health = rookHealth(health);
+						}
+					}
+				
+					//multiplier!!
+					boon = boon * mod
+					build = build * mod
+					t1 = t1 * mod
+					t2 = t2 * mod
+					t3 = t3 * mod
+					t4 = t4 * mod
+					t5 = t5 * mod
+					t6 = t6 * mod
+					t7 = t7 * mod
+				
+					//display
+					message.reply('```for killing whatever you killed, you have obtained:\nBoon = ' + boon + '\nBG = ' + build.toFixed(0) +
+							'\nT1 = ' + t1.toFixed(0) + '\nT2 = ' + t2.toFixed(0) + '\nT3 = ' + t3.toFixed(0) + '\nT4 = ' + t4.toFixed(0) +
+							'\nT5 = ' + t5.toFixed(0) + '\nT6 = ' + t6.toFixed(0) + '\nT7 = ' + t7.toFixed(0) + '\nHealth Gel = ' + health + '```')
 				}
 			break;
-			default:
-				message.reply('Use an actual command ~~ya gosh darned cunt~~');
-		}
 			case 'custom':
 				//%custom [# killed] [Tier] [# of boon dice]d[dice value] [# of grist dice]d[dice value]
 				//%custom 20 9 100d100 100d100
